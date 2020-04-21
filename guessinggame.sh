8b03cf9691906baa23181bdd36fec2a6ba03102b
jfhinchcliffe <hinchy@gmail.com>
2020-04-21 13:04:11 +1000

    refactor guessing game to be a bit more readable

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,34 @@
+
 function main {
   correct_guess=0
-  
-  number_of_files=$(ls | wc -l)
+  actual_number_of_files=$(ls | wc -l)
+
   while [[ $correct_guess -lt 1 ]]
   do
     echo "How many files are in the current directory?"
-    read guess
+    read guessed_number_of_files
 
-    if [[ $guess -eq $number_of_files ]]
+    if guessIsCorrect $guessed_number_of_files $actual_number_of_files
     then
       correct_guess=1
-      echo "Correct"
+      echo "Correct guess!"
     else
-      [[ $guess -gt $number_of_files ]] && echo "Try a lower number" || echo "Try a higher number"
+      failedGuessMessage $guessed_number_of_files $actual_number_of_files
     fi
   done
+}
+
+function failedGuessMessage {
+  [[ $1 -gt $2 ]] && echo "Try a lower number" || echo "Try a higher number"
+}
+
+function guessIsCorrect {
+  if [[ $1 -eq $2 ]]
+  then
+    return 0
+  else
+    return 1
+  fi
 }
 
 main
